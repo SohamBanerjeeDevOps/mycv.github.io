8847e03fad31919a044e26ec982756d55602d589
Soham Banerjee <153107137+SohamBanerjeeDevOps@users.noreply.github.com>
2023-12-10 21:27:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
 js = "amazing";
-if (js === "amazing") alert("Thank you for your dedicating time to review my resume.:)");
+if (js === "amazing") alert( "Thank you for your dedicating time to review my resume");
